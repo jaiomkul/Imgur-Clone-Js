4a4b16fdadc4092cdf14da59e24ac8858cfebaa9
jaiomkul <jaygupta2601@gmail.com>
2022-07-18 17:08:22 +0530

    done

---
+++ src/script/main.js
@@ -0,0 +1,41 @@
+// import './style.css'
+
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
+
+import navbar from "../components/navBar.js";
+import logoImg from "../svg/svgImg.js";
+import { home } from "../components/home.js";
+import { makeApiCall, appendData } from "./async.js";
+
+document.querySelector("#head-nav").innerHTML = navbar();
+
+document.querySelector(".logoImg").innerHTML = logoImg();
+
+document.querySelector("#homePage").innerHTML = home();
+
+let key = "IbQfsT5hSUQZzIi1vvRrSMZJ2Tbk8W6wd7lOEFIDR2Y";
+let url = `https://api.unsplash.com/photos/?client_id=${key}`;
+
+let image_data = document.querySelector(".home02");
+let image_data1 = document.querySelector(".home01");
+
+let res = await makeApiCall(url);
+console.log("Res", res);
+
+appendData(res, image_data);
+appendData(res, image_data1);
+
+//Search Bar
+let input = document.getElementById("Searchbar");
+
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    let input_value = document.getElementById("Searchbar").value;
+
+    localStorage.setItem("search_term", input_value);
+    window.location.href = "./src/html/navSearch.html";
+  }
+});
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}", "./index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
